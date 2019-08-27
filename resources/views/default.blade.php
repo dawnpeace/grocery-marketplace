9237c01fc370d31298d7b9c9f4984629f2cec60c
All Binardo Vandikha <all_binardo@rocketmail.com>
2019-08-27 19:05:01 +0700

    change app.js placement

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -6,10 +6,6 @@
     <title>{{config("app.name")}}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{asset('js/app.js')}}"></script>
-    <script>
-      // turbolinks.start();
-    </script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
@@ -116,6 +112,7 @@
           Dapurpedia &copy; 2019 
         </div>
       </footer>
+      <script src="{{asset('js/app.js')}}"></script>
       <script>
         $(document).ready(function() {
           $(".dropdown-toggle").dropdown();
